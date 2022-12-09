b2f66cde8c27e9070928cb53cfce521a1834a60e
Samarth Srinivasa <samarthsrinivasa@samarthsmbp7541.attlocal.net>
2022-12-08 20:57:49 -0800

    Fixed backwards Elimination, fixing some last quirks

--- project2.py
+++ project2.py
@@ -24,13 +24,8 @@ rowLength = len(data[0])
 current_set_of_features = []
 best_set = []
 
-
 def leave_one_out_cross_validation(data, current_set, feature_to_add):
     number_correctly_classified = 0
-
-    #remove later or change:
-    if not current_set and feature_to_add == 0: return 0.5
-    #-------------------------
 
     for i in range(columnLength):
     #object_to_classify = df2.iloc[i]
@@ -46,20 +41,16 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
         for k in range(columnLength):
             sets_to_add = []
 
-            #remove later or change:
             if feature_to_add != 0:
                 sets_to_add.append(data[i][feature_to_add] - data[k][feature_to_add])
             for j in range(len(current_set)):
                 sets_to_add.append(data[i][current_set[j]] - data[k][current_set[j]])
-            #----------------------
 
         #print("Ask if " + str(i) + " is nearest neighbors with " + str(k))
             if k != i and sets_to_add:
 
-                #remove later or change:
                 sets_to_add = np.square(sets_to_add)
                 distance = np.sqrt(sum((sets_to_add)))
-                 #----------------------
 
                 #distance = math.sqrt(sum((sets_to_add)**2))
                     #print(distance)
@@ -82,7 +73,7 @@ lastAcc = 0
 
 for i in range(1, rowLength):
     print("Current Set of Features: ", current_set_of_features)
-    print("On the " + str(i) + "th level of the search tree")
+    #print("On the " + str(i) + "th level of the search tree")
     #feature_to_add_at_this_level = 0
 
     best_so_far_accuracy = 0
@@ -92,7 +83,7 @@ for i in range(1, rowLength):
         if k not in current_set_of_features:
             #print("--Considering adding the " + str(k) + " feature")
             accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
-            print("Accuracy of Features ", current_set_of_features, " with ", k, "added:", accuracy)
+            #print("Accuracy of Features ", current_set_of_features, " with ", k, "added:", accuracy)
         if accuracy > best_so_far_accuracy:
             best_so_far_accuracy = accuracy
             feature_to_add_at_this_level = k
@@ -105,10 +96,67 @@ for i in range(1, rowLength):
 
     print("On level " + str(i) + " I added feature " + str(feature_to_add_at_this_level) + " to current set, accuracy: ", best_so_far_accuracy)    
 
+    if best_so_far_accuracy > bestAcc:
+        bestAcc = best_so_far_accuracy
+        best_set = deepcopy(current_set_of_features)
+    if lastAcc > best_so_far_accuracy:
+        print("Accuracy has decreased!\n")
+    lastAcc = best_so_far_accuracy
+
+print("This is the best set for forward selection: ", best_set)
+
+
+current_set_of_features = []
+best_set = []
+
+bestAcc = 0
+lastAcc = 0
+
+for i in range(1, rowLength):
+    current_set_of_features.append(i)
+
+#backwards elimination
+for i in range(1, rowLength):
+
+    print("Current Set of Features: ", current_set_of_features)
+    print("On the " + str(i) + "th level of the search tree")
+    #feature_to_add_at_this_level = 0
+
+    best_so_far_accuracy = 0
+    removableFeat = 0
+
+    for k in current_set_of_features:
+
+        eliminationQueue = deepcopy(current_set_of_features)
+        eliminationQueue.remove(k)
+
+        back_accuracy = leave_one_out_cross_validation(data, eliminationQueue, 0)
+        
+        #if k not in current_set_of_features:
+            #print("--Considering adding the " + str(k) + " feature")
+            #accuracy = leave_one_out_cross_validation(data, current_set_of_features, k)
+            #print("Accuracy of Features ", current_set_of_features, " with ", k, "added:", accuracy)
+        if back_accuracy > best_so_far_accuracy:
+            best_so_far_accuracy = back_accuracy
+            removableFeat = k
+            #print(best_so_far_accuracy) 
+            #print(current_set_of_features)           
+
+    #current_set_of_features[i] = feature_to_add_at_this_level
+    #if feature_to_add_at_this_level != 0:
+    #current_set_of_features.append(feature_to_add_at_this_level)
+
+    print("On level " + str(i) + " I removed feature " + str(removableFeat) + " to current set, accuracy: ", best_so_far_accuracy)    
+
+    if current_set_of_features: 
+        current_set_of_features.remove(removableFeat)
 
     if best_so_far_accuracy > bestAcc:
         bestAcc = best_so_far_accuracy
         best_set = deepcopy(current_set_of_features)
     if lastAcc > best_so_far_accuracy:
-        print("Accuracy has decreased lol\n")
+        print("Accuracy has decreased!\n")
     lastAcc = best_so_far_accuracy
+
+
+print("This is the best set for backwards elimination: ", best_set)
--- testDataFrame.py
+++ testDataFrame.py
@@ -1,47 +1,24 @@
-import pandas as pd
-import math
-
-data = pd.read_csv('smaller.txt', header = None, delim_whitespace=True)
-df = pd.DataFrame(data)
-df2 = df.iloc[: , 1:]
-y = len(df[1])
-x = len(df2.columns)
-
-#for row in df.itertuples():
-    #print(math.sqrt(sum((row[0] - row[1:6])**2)))
-    #print(row[1] - row[2:3])
-
-#for row in df.itertuples():
-    #distance = math.sqrt(sum((row[0] - row[1:index])**2))
-    #print(distance)
-
-distance = math.sqrt(sum((row[0] - row[1:k])**2))
+import math 
+import random
+import numpy as np
 
 
-def leave_one_out_cross_validation(data, current_set, feature_to_add):
-    number_correctly_classified = 0
-    for i in range(y):
-    #object_to_classify = df2.iloc[i]
-        object_to_classify = 0
-        label_object_to_classify = df[0].values[i]
+data1 = []
+with open("CS170_Small_Data__123.txt", "r") as file:
+    for line in file:
+        data1.append(line.split())
 
-        nearest_neighbor_distance = math.inf
-        nearest_neighbor_location = math.inf
-        nearest_neighbor_label = 0
-        for k in range(y):
-        #print("Ask if " + str(i) + " is nearest neighbors with " + str(k))
-            if k != i:
-                distance = 0
-                for index, row in df.iterrows():
-                    distance = math.sqrt(sum((row[0] - row[1:k])**2))
-                if distance < nearest_neighbor_distance:
-                    nearest_neighbor_distance = distance
-                    nearest_neighbor_location = k
-                    nearest_neighbor_label = df[0].values[nearest_neighbor_location]
-           
-    #print("Object " + str(i) + " is class " + str(label_object_to_classify))    
-        if (label_object_to_classify == nearest_neighbor_label):
-            number_correctly_classified += 1
+data2 = np.array(data1)
+data = data2.astype(float)
+#data = data1.astype(np.float)
 
-    accuracy = number_correctly_classified/y 
-    return accuracy 
+columnLength = len(data)
+rowLength = len(data[0])
+
+current_set_of_features = []
+best_set = [1,2,3,4,5,6,23,423,4]
+
+best_set = np.square(best_set)
+distance = np.sqrt(sum((best_set)))
+
+print(distance)
